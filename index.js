0092037257675ddae46ba5cb1f3090cbe4b8133c
wintermute2015 <wintermute2015@ukr.net>
2015-11-19 13:39:45 +0200

    added angular

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var multer = require('multer');
+var fs = require('fs');
+
 var app = express();
 	
 app.use(multer({ dest: './uploads/',
@@ -18,8 +20,14 @@ app.get('/',function(req,res){
     res.sendFile(__dirname + "/index.html");
 });
 
+app.use(express.static(__dirname + '/'));
+
 app.post('/upload', function (req, res) {
     res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/api/list', function (req, res) {
+    res.send(fs.readdirSync('./uploads'));
 });
 
 app.listen(3000,function(){
